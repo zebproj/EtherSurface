b8c3ae3ff4f8a386684f72e0f3b050417db8f294
Paul Batchelor <pbatchelor@berklee.edu>
2014-03-20 08:34:49 -0400

    multitouch visually acceptable

--- src/com/example/multitouchexample/MultiTouchView.java
+++ src/com/example/multitouchexample/MultiTouchView.java
@@ -16,7 +16,7 @@ import android.view.View;
 
 
 public class MultiTouchView extends View{
-	private static int SIZE = 60;
+	private static int SIZE = 70;
 	private class Circle extends PointF{
 		boolean isVisible = false;
 	}
@@ -31,7 +31,8 @@ public class MultiTouchView extends View{
 	boolean isVisible[] = new boolean[10];
 	int totalPointers = 0;
 	int touchIds[] = new int[10];
-
+	float touchX[] = new float[10];
+	float touchY[] = new float[10];
 	
 	public MultiTouchView(Context context, AttributeSet attrs){
 		super(context, attrs);
@@ -51,7 +52,13 @@ public class MultiTouchView extends View{
 		circlePaint = new Paint();
 		circlePaint.setColor(Color.rgb(233, 214, 107));
 		circlePaint.setStyle(Paint.Style.FILL);
-		//for(int i = 0; i < 10; i++) isVisible[i] = false;
+		for(int i = 0; i < 10; i++) isVisible[i] = false;
+		
+		for(int i = 0; i < touchIds.length; i++) {
+			touchIds[i] = -1;
+			touchX[i] = -1;
+			touchY[i] = -1;
+		}	
 
 	}
 	
@@ -59,49 +66,66 @@ public class MultiTouchView extends View{
 	@Override
 	public boolean onTouchEvent(MotionEvent event){
 		int pointerIndex = event.getActionIndex();
-		int pointerId = event.getPointerId(pointerIndex);
+		//int pointerId = event.getPointerId(pointerIndex);
 		int maskedAction = event.getActionMasked();
 		
 		switch(maskedAction){
 		case MotionEvent.ACTION_DOWN:
 			
 		case MotionEvent.ACTION_POINTER_DOWN: {
-			Log.d("touches", "Action_Pointer_Down");
-			Circle f = new Circle();
-			f.x = event.getX();
-			f.y = event.getY();
-			f.isVisible = true;
-			mActivePointers.put(pointerId, f);
-			totalPointers++;
+			for(int i = 0; i < event.getPointerCount(); i++) {
+				int pointerId = event.getPointerId(i);
+				int id = getTouchId(pointerId);
+				
+				if(id == -1) {
+					
+					id = getTouchIdAssignment();
+					
+					if(id != -1) {
+						touchIds[id] = pointerId;
+						touchX[id] = event.getX(i);
+						touchY[id] = event.getY(i);
+						isVisible[id] = true;
+
+					}
+				}
+				
+			}
 			break;
 		}
 		case MotionEvent.ACTION_MOVE:{
-			for(int size = event.getPointerCount(), i = 0; i < size; i++){
-				PointF point = mActivePointers.get(event.getPointerId(i));
-				if(point != null) {
-					point.x = event.getX(i);
-					point.y = event.getY(i);
+//			for(int size = event.getPointerCount(), i = 0; i < size; i++){
+//				PointF point = mActivePointers.get(event.getPointerId(i));
+//				if(point != null) {
+//					point.x = event.getX(i);
+//					point.y = event.getY(i);
+//				}
+//			}
+//			break;
+			for(int i = 0; i < event.getPointerCount(); i++) {
+				int pointerId = event.getPointerId(i);
+				int id = getTouchId(pointerId);
+
+				if(id != -1) {
+					touchX[id] = event.getX(i);
+					touchY[id] = event.getY(i);
 				}
-			}
-			break;
-		}		
-		case MotionEvent.ACTION_POINTER_UP:{
-			Log.d("touches", "Action_Pointer_UP");
-			totalPointers--;
-			for(int size = event.getPointerCount(), i = 0; i < size; i++){
-				Circle point = mActivePointers.get(event.getPointerId(i));
-				if(point != null) {
-					point.isVisible = true;
-				}
-			}
+				
+		}
 			break;
 		}
+		case MotionEvent.ACTION_POINTER_UP:{
+
+		}
 		case MotionEvent.ACTION_UP: {
-			totalPointers--;
-			Log.d("touches", "Action_UP");
-			Circle point = mActivePointers.get(event.getPointerId(0));
-			point.isVisible = false;
-			break;
+			int activePointerIndex = event.getActionIndex();
+			int pointerId = event.getPointerId(activePointerIndex);
+				
+			int id = getTouchId(pointerId);
+			if(id != -1) {
+				touchIds[id] = -1;
+				isVisible[id] = false;
+			}
 		}
 		
 		}
@@ -118,14 +142,13 @@ public class MultiTouchView extends View{
 		for(int size = mActivePointers.size(), i = 0; i < size; i++){
 	
 		}
-		for(int i = 0; i < mActivePointers.size(); i++){
-			Circle point = mActivePointers.valueAt(i);
-			if(point != null && point.isVisible){
+		for(int i = 0; i < 10; i++){
+			if(isVisible[i]){
 				//mPaint.setColor(colors[i % 9]);
-				canvas.drawCircle(point.x, point.y, SIZE, circlePaint);
+				canvas.drawCircle(touchX[i], touchY[i], SIZE, circlePaint);
 			}
 		}
-		canvas.drawText("Total Pointers: " + totalPointers, 10, 40, textPaint);
+		//canvas.drawText("Total Pointers: " + totalPointers, 10, 40, textPaint);
 
 	}
 	
