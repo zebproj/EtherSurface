0bb48018c6a663ad389e2fab6a23ad9f7526483a
Paul Batchelor <pbatchelor@berklee.edu>
2014-03-27 04:42:38 -0400

    added new icons

---
+++ make_icons.sh
@@ -0,0 +1,9 @@
+function make_icon {
+	inkscape -z -e ic_launcher.png -w $1 -h $1 icon.svg
+	mv ic_launcher.png $2
+}
+
+make_icon 72 res/drawable-hdpi
+make_icon 48 res/drawable-mdpi
+make_icon 96 res/drawable-xhdpi
+make_icon 144 res/drawable-xxhdpi
