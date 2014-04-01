27c0d6a28223eb8c47c5b3188bfaaa3afb943484
Paul Batchelor <pbatchelor@berklee.edu>
2014-03-31 23:16:25 -0400

    Updates

--- make_icons.sh
+++ make_icons.sh
@@ -7,3 +7,5 @@ make_icon 72 res/drawable-hdpi
 make_icon 48 res/drawable-mdpi
 make_icon 96 res/drawable-xhdpi
 make_icon 144 res/drawable-xxhdpi
+	
+inkscape -z -e icon_hires.png -w 512 -h 512 icon.svg
--- sign.sh
+++
@@ -1,1 +0,0 @@
-jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore release-key.keystore EtherPad.apk zebproj
